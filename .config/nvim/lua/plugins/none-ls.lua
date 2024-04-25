0c7ab9822b1ccadd2432d088f9ca7575be9f5f17
Tyler Fisher <11068657+tfisher21@users.noreply.github.com>
2024-04-25 12:18:35 -0500

    Add go support

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,33 +1,47 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local none_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local none_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    none_ls.setup({
-      sources = {
-        none_ls.builtins.formatting.stylua,
-        -- none_ls.builtins.diagnostics.rubocop, -- Doesn't work in current Molecule environment due to version of rubocop used
-        none_ls.builtins.formatting.rubocop,
-        none_ls.builtins.diagnostics.eslint_d,
-        none_ls.builtins.formatting.prettier.with({
-          filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact", "css", "scss", "json", "yaml", "markdown" },
-        }),
-      },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ async = false })
-            end,
-          })
-        end
-      end,
-    })
+		none_ls.setup({
+			sources = {
+				none_ls.builtins.formatting.stylua,
+				-- none_ls.builtins.diagnostics.rubocop, -- Doesn't work in current Molecule environment due to version of rubocop used
+				none_ls.builtins.formatting.rubocop,
+				none_ls.builtins.diagnostics.eslint_d,
+				none_ls.builtins.formatting.prettier.with({
+					filetypes = {
+						"javascript",
+						"typescript",
+						"javascriptreact",
+						"typescriptreact",
+						"css",
+						"scss",
+						"json",
+						"yaml",
+						"markdown",
+					},
+				}),
+				none_ls.builtins.diagnostics.golangci_lint,
+				none_ls.builtins.formatting.goimports_reviser,
+				none_ls.builtins.formatting.gofumpt,
+				none_ls.builtins.formatting.golines,
+			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
+			end,
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,27 +1,66 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    end
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    config = function()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      }
-      require("telescope").load_extension("ui-select")
-    end
-  }
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0" },
+		},
+		config = function()
+			local telescope = require("telescope")
+			local lga_actions = require("telescope-live-grep-args.actions")
+
+			telescope.setup({
+				extensions = {
+					live_grep_args = {
+						auto_quoting = true, -- enable/disable auto-quoting
+						-- define mappings, e.g.
+						mappings = { -- extend mappings
+							i = {
+								["<C-k>"] = lga_actions.quote_prompt(),
+								["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+							},
+						},
+					},
+				},
+			})
+
+			require("telescope").load_extension("live_grep_args")
+		end,
+		keys = {
+			{
+				"<leader>ff",
+				"<cmd>lua require('telescope.builtin').find_files()<cr>",
+				desc = "Find Files",
+			},
+			{
+				"<leader>fg",
+				"<cmd>lua require('telescope.builtin').live_grep()<cr>",
+				desc = "Live Grep",
+			},
+			{
+				"<leader>fb",
+				"<cmd>lua require('telescope.builtin').buffers()<cr>",
+				desc = "Buffers",
+			},
+			{
+				"<leader>fr",
+				"<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>",
+				desc = "Live Grep Args",
+			},
+		},
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
