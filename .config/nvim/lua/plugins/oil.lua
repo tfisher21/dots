b092d4337f3b41d0be3dfb0604dc6f37b7fb1494
Tyler Fisher <11068657+tfisher21@users.noreply.github.com>
2024-08-29 12:55:01 -0500

    Switch from neotree to oil

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/oil.nvim",
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  config = function()
+    require("oil").setup({})
+  end,
+  keys = {
+    {
+      "<leader>n",
+      "<CMD>Oil --float<CR>",
+    },
+  },
+}
