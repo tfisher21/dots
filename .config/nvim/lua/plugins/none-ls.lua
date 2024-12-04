e7bc776d26cd94f3b6d93d99eb48c7765354b6b2
Tyler Fisher <11068657+tfisher21@users.noreply.github.com>
2024-12-04 09:05:18 -0600

    Migrate towards Ruby-LSP

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,44 +1,44 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local none_ls = require("null-ls")
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local none_ls = require("null-ls")
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-		none_ls.setup({
-			sources = {
-				-- Lua Support
-				none_ls.builtins.formatting.stylua,
+    none_ls.setup({
+      sources = {
+        -- Lua Support
+        none_ls.builtins.formatting.stylua,
 
-				-- Ruby Support
-				none_ls.builtins.diagnostics.rubocop,
-				none_ls.builtins.formatting.rubocop,
+        -- Ruby Support
+        -- none_ls.builtins.diagnostics.rubocop,
+        -- none_ls.builtins.formatting.rubocop,
 
-				-- Golang Support
-				none_ls.builtins.diagnostics.golangci_lint,
-				none_ls.builtins.formatting.gofumpt,
-				none_ls.builtins.formatting.goimports,
-				none_ls.builtins.formatting.goimports_reviser,
-				none_ls.builtins.formatting.golines,
+        -- Golang Support
+        none_ls.builtins.diagnostics.golangci_lint,
+        none_ls.builtins.formatting.gofumpt,
+        none_ls.builtins.formatting.goimports,
+        none_ls.builtins.formatting.goimports_reviser,
+        none_ls.builtins.formatting.golines,
 
-				-- Javascript & Typescript Support
-				none_ls.builtins.diagnostics.eslint,
-				-- none_ls.builtins.diagnostics.eslint_d,
-				none_ls.builtins.formatting.prettier,
-			},
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ async = false })
-						end,
-					})
-				end
-			end,
-		})
+        -- Javascript & Typescript Support
+        none_ls.builtins.diagnostics.eslint,
+        -- none_ls.builtins.diagnostics.eslint_d,
+        none_ls.builtins.formatting.prettier,
+      },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
