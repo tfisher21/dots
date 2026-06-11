aeab601c4c96aa20eafccbcc6b775cb0af9fc1e6
Tyler Fisher <trfisher21@protonmail.com>
2026-06-11 09:38:50 -0500

    feat(nvim): enable mini.icons module

--- stow_home/.config/nvim/lua/plugins/mini.lua
+++ stow_home/.config/nvim/lua/plugins/mini.lua
@@ -2,10 +2,11 @@ vim.pack.add({
   'https://www.github.com/echasnovski/mini.nvim',
 })
 
-require("mini.ai").setup({})
-require("mini.bracketed").setup({})
-require("mini.comment").setup({})
-require("mini.pairs").setup({})
-require("mini.splitjoin").setup({})
-require("mini.statusline").setup({})
+require("mini.ai").setup()
+require("mini.bracketed").setup()
+require("mini.comment").setup()
+require("mini.icons").setup()
+require("mini.pairs").setup()
+require("mini.splitjoin").setup()
+require("mini.statusline").setup()
 -- TODO: More Mini modules?
