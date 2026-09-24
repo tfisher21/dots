8f99627895db3bc91536e6521cffcaf47b702ab9
Tyler Fisher <trfisher21@protonmail.com>
2026-09-24 13:57:52 -0500

    feat(nvim): enable treesitter for c and java filetypes

--- stow_home/.config/nvim/lua/plugins/treesitter.lua
+++ stow_home/.config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,6 @@ vim.pack.add({ { src = "https://github.com/nvim-treesitter/nvim-treesitter"} })
 require('nvim-treesitter').install { 'rust', 'go', 'lua', 'ruby', 'vim' }
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { "go", "lua", "ruby", "rust", "vim" },
+  pattern = { "c", "go", "java", "lua", "ruby", "rust", "vim" },
   callback = function() vim.treesitter.start() end,
 })
